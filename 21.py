f22db13826d402a95a6a99c633b2e6f5acaaf6c1
Mahadev Mondal <mondalmahadev816@gmail.com>
2025-06-21 20:25:12 +0530

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,16 @@
+
+marks=[]
+f1=int(input("Enter the first student marks: "))
+marks.append(f1)
+f2=int(input("Enter the second student marks: ")) 
+marks.append(f2)
+f3=int(input("Enter the third student marks: "))
+marks.append(f3)
+f4=int(input("Enter the fourth student marks: "))  
+marks.append(f4)
+f5=int(input("Enter the fifth student marks: ")) 
+marks.append(f5)
+f6=int(input("Enter the sixth student marks: "))
+marks.append(f6)
+marks.sort()  
+print("The marks you entered are: ", marks)
