f20e82aa4492270871b396899f01cd7d310d6575
Mahadev Mondal <mondalmahadev816@gmail.com>
2025-07-08 19:22:16 +0530

    Add files via upload

---
+++ 135.py
@@ -0,0 +1,5 @@
+import random
+d=int(input("enter the number:-"))
+f= random.randint(1,45)
+
+print(f+d)
---
+++ 136.py
@@ -0,0 +1,12 @@
+import random
+while True:
+  p=int(input("enter the no:-"))
+  d=random.randint(1,30)
+  print(p-d)
+  s=(p-d)
+  print(s)
+  
+  if(s==0):
+    print("done")
+    break
+
